2be7726795df44d5e7d142f23067bc5d9fba51bc
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-05-30 16:15:33 -0400

    More items

--- src/pocketmine/item/SpruceDoor.php
+++ src/pocketmine/item/SpruceDoor.php
@@ -23,9 +23,9 @@ namespace pocketmine\item;
 
 use pocketmine\block\Block;
 
-class WoodenDoor extends Door{
+class SpruceDoor extends Door{
 	public function __construct($meta = 0, $count = 1){
-		$this->block = Block::get(Item::WOODEN_DOOR_BLOCK);
-		parent::__construct(self::WOODEN_DOOR, 0, $count, "Wooden Door");
+		$this->block = Block::get(Item::SPRUCE_DOOR_BLOCK);
+		parent::__construct(self::SPRUCE_DOOR, 0, $count, "Spruce Door");
 	}
 }
